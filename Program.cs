052c6b52889a73d78fd29c64e2b34bdf5f7fe333
coding-jhuunn <codingatjhunn@gmail.com>
2024-03-11 16:05:29 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        class Wizard(string _uname,string _magicName){
+            public string uname = _uname;
+            public string magicName = _magicName;
+        }
+
+        static void Main(string[] args)
+        {
+          Wizard wizard01 = new Wizard("ME","One");
+          
+          Console.ReadKey();
+        }
+    }
+}
